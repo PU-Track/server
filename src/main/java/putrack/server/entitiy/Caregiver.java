083fab4029f944caa82541e656be71bc71a1fbf0
13155a1 <ppy040@gmail.com>
2025-05-16 23:56:33 +0900

    feat: #25 add entities

---
+++ src/main/java/putrack/server/entitiy/Caregiver.java
@@ -0,0 +1,24 @@
+package putrack.server.entitiy;
+import jakarta.persistence.*;
+import java.util.List;
+
+
+
+@Entity
+public class Caregiver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer caregiverId;
+
+    @Column(nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @ManyToMany(mappedBy = "caregivers")
+    private List<Patient> patients;
+}
---
+++ src/main/java/putrack/server/entitiy/Prediction.java
@@ -0,0 +1,20 @@
+package putrack.server.entitiy;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Prediction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer predictionId;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime startTime;
+    private Integer predictionTime;
+}
